0c9e47ba5bc7311ac95e2ea2ec846e47b20d262f
cljang <clayton.jang@gmail.com>
2021-10-23 19:50:57 -0700

    Use hashing for passwords with MD5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 // ========================= Setup ========================= 
 // Create express app
@@ -26,9 +26,6 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-// We got an expletive secret now - secret stored in .env file
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
 
 // ========================= GET/POST ========================= 
@@ -43,7 +40,7 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser) {
     if(err) {
@@ -67,7 +64,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
